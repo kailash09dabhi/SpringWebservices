52b80eafc8b9c90bf5a1928c51c1d15518d34994
Kailash Dabhi <v-kailas.dabhi@decurtis.com>
2023-09-04 19:11:41 +0530

    Add in-memory user details for authentication of employee service

--- employee-service/src/main/java/com/kd/employeeservice/Configurations.java
+++ employee-service/src/main/java/com/kd/employeeservice/Configurations.java
@@ -3,6 +3,12 @@ package com.kd.employeeservice;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -11,8 +17,20 @@ public class Configurations {
     public ModelMapper modalMapper() {
         return new ModelMapper();
     }
+
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails userDetails = User.builder().username("kailas").password(passwordEncoder().encode("dabhi")).roles("ADMIN").build();
+        return new InMemoryUserDetailsManager(userDetails);
     }
 }
