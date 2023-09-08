d5706697782336608a1e3eda3972a3f8529e9c18
Kailash Dabhi <v-kailas.dabhi@decurtis.com>
2023-09-08 20:42:23 +0530

    Add Request and response classes for login,signup and token refresh

---
+++ employee-service/src/main/java/com/kd/employeeservice/auth/RoleType.java
@@ -0,0 +1,7 @@
+package com.kd.employeeservice.auth;
+
+public enum RoleType {
+  ROLE_USER,
+  ROLE_MODERATOR,
+  ROLE_ADMIN
+}
---
+++ employee-service/src/main/java/com/kd/employeeservice/auth/request/TokenRefreshRequest.java
@@ -0,0 +1,17 @@
+package com.kd.employeeservice.auth.request;
+
+
+import jakarta.validation.constraints.NotBlank;
+
+public class TokenRefreshRequest {
+  @NotBlank
+  private String refreshToken;
+
+  public String getRefreshToken() {
+    return refreshToken;
+  }
+
+  public void setRefreshToken(String refreshToken) {
+    this.refreshToken = refreshToken;
+  }
+}
